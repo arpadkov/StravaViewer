cd4486d7aade3b6d8bab0a1444f7c74fd15057fb
mihalyl <lelkes.mihaly15@gmail.com>
2022-05-22 11:29:25 +0200

    2 display time buttons added

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -31,21 +31,47 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.BarPlot = new ScottPlot.FormsPlot();
+            this.NextTimeButton = new System.Windows.Forms.Button();
+            this.LastTimeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BarPlot
             // 
-            this.BarPlot.Location = new System.Drawing.Point(24, 27);
+            this.BarPlot.Location = new System.Drawing.Point(138, 36);
             this.BarPlot.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.BarPlot.Name = "BarPlot";
             this.BarPlot.Size = new System.Drawing.Size(467, 346);
             this.BarPlot.TabIndex = 0;
+            // 
+            // NextTimeButton
+            // 
+            this.NextTimeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NextTimeButton.Image = global::StravaViewer.Properties.Resources.button_right;
+            this.NextTimeButton.Location = new System.Drawing.Point(549, 375);
+            this.NextTimeButton.Name = "NextTimeButton";
+            this.NextTimeButton.Size = new System.Drawing.Size(56, 53);
+            this.NextTimeButton.TabIndex = 1;
+            this.NextTimeButton.UseVisualStyleBackColor = true;
+            this.NextTimeButton.Click += new System.EventHandler(this.NextTimeButton_Click);
+            // 
+            // LastTimeButton
+            // 
+            this.LastTimeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LastTimeButton.Image = global::StravaViewer.Properties.Resources.button_left;
+            this.LastTimeButton.Location = new System.Drawing.Point(166, 375);
+            this.LastTimeButton.Name = "LastTimeButton";
+            this.LastTimeButton.Size = new System.Drawing.Size(56, 53);
+            this.LastTimeButton.TabIndex = 1;
+            this.LastTimeButton.UseVisualStyleBackColor = true;
+            this.LastTimeButton.Click += new System.EventHandler(this.LastTimeButton_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.LastTimeButton);
+            this.Controls.Add(this.NextTimeButton);
             this.Controls.Add(this.BarPlot);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(1500, 1500);
@@ -61,5 +87,7 @@
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private ScottPlot.FormsPlot BarPlot;
+        private Button NextTimeButton;
+        private Button LastTimeButton;
     }
 }
